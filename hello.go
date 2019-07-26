519ead94d3dd2d49fdb30fbf63001865b28828e1
Josh Laird <josh.laird@condenastint.com>
2019-07-26 21:01:44 +0100

    Final Hello, World refactor

--- hello.go
+++ hello.go
@@ -2,15 +2,32 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(recipient string) string {
+func Hello(recipient, language string) string {
 	if recipient == "" {
 		recipient = "World"
 	}
-	return englishHelloPrefix + recipient
+
+	return greetingPrefix(language) + recipient
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
