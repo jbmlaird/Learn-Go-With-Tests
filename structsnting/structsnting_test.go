b1372ab2d8d76e96cf234ee44cc5fb2f901d0e1d
Josh Laird <josh.laird@condenastint.com>
2019-07-27 12:37:31 +0100

    Table driven test

--- structsnting/structsnting_test.go
+++ structsnting/structsnting_test.go
@@ -15,24 +15,20 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("correctly calc Rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		got := rectangle.Area()
-		want := 100.0
 
-		if got != want {
-			t.Errorf("Expected %.2f, got %.2f", want, got)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10.0, 10.0}, 100.0},
+		{Circle{10.0}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
 		}
-	})
-
-	t.Run("correctly calc Circles", func(t *testing.T) {
-		circle := Circle{10.0}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
+	}
 
 }
