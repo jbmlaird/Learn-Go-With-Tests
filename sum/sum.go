8c2be5b4b3923e3d6f9d9446bc2c1dd3d4a35725
Josh Laird <josh.laird@condenastint.com>
2019-07-27 12:13:48 +0100

    SumAllTails

--- sum/sum.go
+++ sum/sum.go
@@ -4,6 +4,18 @@ func Sum(numbers []int) (sum int) {
 	for _, value := range numbers {
 		sum += value
 	}
-
 	return sum
+}
+
+func SumAllTails(slicesToSum ...[]int) []int {
+	var sumSlice []int
+	for _, numbers := range slicesToSum {
+		if len(numbers) > 0 {
+			tail := numbers[1:]
+			sumSlice = append(sumSlice, Sum(tail))
+		} else {
+			sumSlice = append(sumSlice, 0)
+		}
+	}
+	return sumSlice
 }
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,6 +1,9 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -13,5 +16,29 @@ func TestSum(t *testing.T) {
 		if want != got {
 			t.Errorf("Wanted '%d' but got '%d', given '%v'", want, got, numbers)
 		}
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Wanted '%v' but got '%v'", want, got)
+		}
+	}
+
+	t.Run("sum all the tails correclty", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{0, 9, 5})
+		want := []int{5, 14}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
 	})
 }
